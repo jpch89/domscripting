9d57cda6cb726ce95855b6a55f0176b2d09121fd
团子大圆帅 <jpch89@outlook.com>
2020-01-29 21:33:03 +0800

    add 10.2.4

---
+++ 10-用JS实现动画效果/02-使用的动画/js/prepareSlideshow.js
@@ -0,0 +1,28 @@
+function prepareSlideshow() {
+  // 确保浏览器支持 dom 方法
+  if (!document.getElementsByTagName) return false
+  if (!document.getElementById) return false
+  // 确保元素存在
+  if (!document.getElementById('linklist')) return false
+  if (!document.getElementById('preview')) return false
+  // 为图片应用样式
+  var preview = document.getElementById('preview')
+  preview.style.position = 'absolute'
+  preview.style.left = '0px'
+  preview.style.top = '0px'
+  // 取得列表中的所有链接
+  var list = document.getElementById('linklist')
+  var links = list.getElementsByTagName('a')
+  // 为 mouseover 事件添加动画效果
+  links[0].onmouseover = function() {
+    moveElement('preview', -100, 0, 10)
+  }
+  links[1].onmouseover = function() {
+    moveElement('preview', -200, 0, 10)
+  }
+  links[2].onmouseover = function() {
+    moveElement('preview', -300, 0, 10)
+  }
+}
+
+addLoadEvent(prepareSlideshow)
